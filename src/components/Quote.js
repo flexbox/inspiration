e7607f37ea086223161fa3ad28083a92da0335ba
David Leuliette <dleuliette@gmail.com>
2018-08-10 15:26:36 +0200

    🗨 First draft for Quote style

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,34 +1,60 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Section = styled.div`
-  font-size: 24px;
-  padding: 30px 0;
+const QuoteList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuoteSection = styled.div`
+  padding: 3rem 0;
+  margin-bottom: 15rem;
   text-align: center;
+  background-color: #373737;
+  border-radius: 5px;
+  color: white;
+  min-height: 40vh;
+  max-width: 700px;
+  display: flex;
+  align-items: center;
+`
+
+const Blockquote = styled.blockquote`
+  width: 100%;
 `
 
 const Title = styled.p`
   font-family: 'Vidaloka', serif;
+  font-size: 44px;
+`
+
+const Hr = styled.hr`
+  max-width: 100px;
+  opacity: 0.8;
+  margin: 2rem auto;
 `
 
 const Cite = styled.cite`
   text-transform: uppercase;
   font-style: normal;
+  font-size: 24px;
+  opacity: 0.6;
+  letter-spacing: 2px;
 `
 
 const Quote = ({ data }) => (
-  <Section>
+  <QuoteList>
     {data.allContentfulQuote.edges.map(edge => (
-      <blockquote>
-        <Title>{edge.node.title.title}</Title>
-        <Cite>
-          {edge.node.author}
-          <small>{edge.node.url_reference}</small>
-          <small>{edge.node.twitter_name}</small>
-        </Cite>
-      </blockquote>
+      <QuoteSection key={edge.node.id}>
+        <Blockquote cite={edge.node.url_reference}>
+          <Title>{edge.node.title.title}</Title>
+          <Hr />
+          <Cite>{edge.node.author}</Cite>
+        </Blockquote>
+      </QuoteSection>
     ))}
-  </Section>
+  </QuoteList>
 )
 
 export default Quote
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import './index.css'
-import Header from '../components/Header'
 import Quote from '../components/Quote'
 
 const Layout = ({ children, data }) => (
@@ -15,18 +14,8 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: data.site.siteMetadata.keywords },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-      <Quote data={data} />
-    </div>
+    <Quote data={data} />
+    {children()}
   </div>
 )
 
@@ -48,8 +37,8 @@ export const query = graphql`
     allContentfulQuote {
       edges {
         node {
+          id
           author
-          twitter_name
           url_reference
           title {
             title
