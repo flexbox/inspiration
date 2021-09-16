4f2b0b8b36c05dfdb517f56e3c192fc00cdf9737
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-09-16 11:29:57 +0200

    remove rebass from 404.js and quoteItems.js

--- src/components/quote/quoteItem.js
+++ src/components/quote/quoteItem.js
@@ -1,71 +1,32 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { Box, Heading, Text } from 'rebass/styled-components'
 
 const QuoteItem = quote => {
   return (
-    <Section className="max-w-screen-md rounded bg-white shadow transition duration-500 ease-in-out transform hover:shadow hover:scale-4">
-      <Blockquote className="text-center m-4 p-4">
-        <Title className="font-vidaloka font-semibold pt-12 pb-6 text-5xl">
+    <div
+      id="Section"
+      className="max-w-screen-md rounded bg-white shadow transition duration-500 ease-in-out transform hover:shadow hover:scale-4"
+    >
+      <div id="blockquote" className="text-center m-4 p-4">
+        <div
+          id="Title"
+          className="font-vidaloka font-semibold pt-12 pb-6 text-5xl"
+        >
           {quote.title}
-        </Title>
+        </div>
         <hr
           className="flex m-auto my-4 p-0 border-0 border-b-2"
           width="100px"
         />
-        <Cite
+        <div
+          id="cite"
           className="uppercase opacity-6 tracking-wider pt-8 pb-12"
           as="cite"
         >
           {quote.author}
-        </Cite>
-      </Blockquote>
-    </Section>
+        </div>
+      </div>
+    </div>
   )
 }
-
-QuoteItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-}
-
-const Section = styled(Box)`
-  max-width: 800px;
-  background-color: ${props => props.theme.colors.sectionBg};
-  border-radius: 5px;
-
-  transition: box-shadow 300ms ease-in-out;
-  box-shadow: ${props => props.theme.shadows.small};
-  &:hover {
-    box-shadow: ${props => props.theme.shadows.xlarge};
-  }
-`
-
-const Blockquote = styled.blockquote`
-  text-align: center;
-  margin: 0;
-  padding: 0;
-
-  hr {
-    max-width: 100px;
-    opacity: 0.2;
-    margin: 2rem auto;
-    border: 0;
-    border-bottom: 2px solid ${props => props.theme.colors.border};
-  }
-`
-
-const Title = styled(Heading)`
-  color: ${props => props.theme.colors.sectionText};
-`
-
-const Cite = styled(Text)`
-  text-transform: uppercase;
-  font-style: normal;
-  opacity: 0.6;
-  letter-spacing: 2px;
-  color: ${props => props.theme.colors.sectionText};
-`
 
 export default QuoteItem
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from 'components/layout'
-import styled from 'styled-components'
-import { Heading } from 'rebass/styled-components'
 
 const NotFoundPage = () => (
   <Layout>
-    <Heading as="h1">NOT FOUND</Heading>
+    <div id="Heading" as="h1">
+      NOT FOUND
+    </div>
     <p>
       Sorry, this page does not exist anymore{' '}
       <span role="img" aria-label="the sadness">
         😢
       </span>
     </p>
-    <HomeLink to="/">Go back home</HomeLink>
+    <Link to="/">Go back home</Link>
   </Layout>
 )
 
 export default NotFoundPage
-
-const HomeLink = styled(Link)`
-  &:hover {
-    text-decoration: none;
-  }
-`
