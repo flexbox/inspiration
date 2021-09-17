840cb7fe35f27d73ac9b649bd86f1da87ac4a071
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-09-17 13:52:09 +0200

    change classname of buttons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -35,13 +35,17 @@ const Sidebar = () => (
     >
       All
     </Link>
-    <div className="flex flex-col mt-2 mb-4 bg-gray-300 text-gray-500 hover:text-black font-bold py-2 px-4 rounded w-full text-center">
-      <a href="mailto:dleuliette@gmail.com?subject=New%20quote&body=title:%20author_name:%20author_twitter:">
+    <div className="flex flex-col mt-2 mb-4 ">
+      <a
+        className="bg-gray-300 text-gray-500 hover:text-black font-bold py-2 px-4 rounded w-full text-center"
+        href="mailto:dleuliette@gmail.com?subject=New%20quote&body=title:%20author_name:%20author_twitter:"
+      >
         Contribute by email
       </a>
     </div>
-    <div className="flex flex-col mt-4 mb-4 bg-blue-500 hover:text-white text-gray-300 font-bold py-2 px-4 rounded w-full text-center">
+    <div className="flex flex-col mt-4 mb-4 ">
       <a
+        className="bg-blue-500 hover:text-white text-gray-300 font-bold py-2 px-4 rounded w-full text-center"
         href="https://displate.com/flexbox/displates"
         rel="noreferrer"
         target="_blank"
