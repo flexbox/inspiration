e864394de1cb9d376eb3aceb28bf57ee8b22ea67
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-09-23 14:38:50 +0200

    try to convert class to function transition.tsx

--- src/components/transition/transition.tsx
+++ src/components/transition/transition.tsx
@@ -2,23 +2,21 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import posed, { PoseGroup } from 'react-pose'
 
-class Transition extends PureComponent {
-  render() {
-    const { children, location } = this.props
+function Transition() {
+  const { children, location } = props
 
-    const RoutesContainer = posed.div({
-      enter: { opacity: 1, delay: 250, delayChildren: 250 },
-      exit: { opacity: 0 },
-    })
+  const RoutesContainer = posed.div({
+    enter: { opacity: 1, delay: 250, delayChildren: 250 },
+    exit: { opacity: 0 },
+  })
 
-    // To enable page transitions on mount / initial load,
-    // use the prop `animateOnMount={true}` on `PoseGroup`.
-    return (
-      <PoseGroup>
-        <RoutesContainer key={location.pathname}>{children}</RoutesContainer>
-      </PoseGroup>
-    )
-  }
+  // To enable page transitions on mount / initial load,
+  // use the prop `animateOnMount={true}` on `PoseGroup`.
+  return (
+    <PoseGroup>
+      <RoutesContainer key={location.pathname}>{children}</RoutesContainer>
+    </PoseGroup>
+  )
 }
 
 Transition.propTypes = {
