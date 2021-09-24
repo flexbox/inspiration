2492a736526c9c2e1b5d58c3fc2ad56bd9728542
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-09-24 11:59:28 +0200

    remove styled-components

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,24 +2,22 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Head from './head'
 import Sidebar from './Sidebar'
-import styled, { ThemeProvider } from 'styled-components'
-import { Box } from 'rebass/styled-components'
 import theme from '../themes/theme'
 
 const Layout = ({ children }) => (
   <>
-    <ThemeProvider className="bg-gray-100" theme={theme}>
+    <div className="bg-gray-100" theme={theme}>
       <>
         <Head />
-        <Graaaadient className="w-full border-t-3 border-solid bg-gray-100" />
-        <Wrapper className="flex">
+        <div className="w-full border-t-3 border-solid bg-gray-100" />
+        <div className="flex">
           <Sidebar />
-          <Content className="overflow-auto bg-gray-100 w-full" p={4}>
+          <div className="overflow-auto bg-gray-100 w-full pt-24 pb-24" p={4}>
             {children}
-          </Content>
-        </Wrapper>
+          </div>
+        </div>
       </>
-    </ThemeProvider>
+    </div>
   </>
 )
 
@@ -37,26 +35,5 @@ const LayoutWithQuery = props => (
     render={data => <Layout data={data} {...props} />}
   />
 )
-
-const Wrapper = styled.div`
-  display: grid;
-  padding: 0;
-  grid-template-columns: 1fr;
-
-  @media (min-width: 550px) {
-    height: 100vh;
-    grid-template-columns: 250px 1fr;
-  }
-`
-
-const Content = styled(Box)`
-  overflow: auto;
-`
-
-const Graaaadient = styled(Box)`
-  height: 3rem;
-  width: 100%;
-  border-top: 3px solid ${props => props.theme.colors.black};
-`
 
 export default LayoutWithQuery
