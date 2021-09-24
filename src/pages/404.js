70a57abe7495154518619f5058fe0230a48be41e
David Leuliette <dleuliette@gmail.com>
2021-09-24 13:39:30 +0200

    Update src/pages/404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
     <div className="mt-20 m-auto flex flex-col max-w-screen-md rounded bg-white shadow transition duration-500 ease-in-out transform hover:shadow hover:scale-4 items-center">
       <div className="m-auto flex flex-col items-center text-center p-4">
         <p className="font-vidaloka text-3xl">
-          Sorry this pas does not exist anymore
+          Sorry, this page does not exist anymore
         </p>
       </div>
       <div className="m-auto flex flex-col items-center">
